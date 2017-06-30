c3c0d0bd42987c12dd2c4a213a58585cef72a7a1
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2017-06-30 23:22:46 +0200

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,7 @@ mainAccordion.accordionElements[3].addEventListener('click', (event) => {
         //Showing loading message.
         event.target.nextElementSibling.innerHTML = 'Loading...';
         const xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = () => {
+        xmlhttp.onreadystatechange = function () {
             //If request is OK
             console.log(this.readyState, this.status);
             if (this.readyState == 4 && this.status == 200) {
